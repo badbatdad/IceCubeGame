0264f106ae844d98ff4291273c06c7ccee776134
badbatdad <filanto637@yandex.ru>
2025-03-02 14:02:15 +0300

    3 commit
    Добавлен скрипт Movement

---
+++ Assets/Scrtpts/Movement.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed = 5f; // Скорость перемещения
+    [SerializeField] private float rotationSpeed = 100f;
+    [SerializeField] private LayerMask _obstacleMask;
+    [SerializeField] private float _step = 0.1f;
+    
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");  
+        float verticalInput = Input.GetAxis("Vertical");    
+
+        Move(horizontalInput, verticalInput, movementSpeed);
+
+        
+    }
+
+    
+    
+    private void Move(float horizontalInput, float verticalInput, float movementSpeed)
+    {
+        Vector3 direction = new Vector3(horizontalInput, 0, verticalInput);
+        if (TryMove(direction))
+        {
+        transform.position = transform.position + new Vector3(horizontalInput * movementSpeed * Time.deltaTime, 0, verticalInput * movementSpeed * Time.deltaTime);
+        }
+    }
+
+    private bool TryMove(Vector3 direction)
+    {
+        var ray = new Ray(transform.position, direction);
+
+        if(Physics.Raycast(ray, _step, _obstacleMask))
+            return false;
+        else
+            return true;
+    }
+    
+}
